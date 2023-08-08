6423ecb17ff3523582205e6bf38f57ed468cacb5
MaksymStepanenko <113137299+MaksymStepanenko@users.noreply.github.com>
2023-08-08 09:56:54 +0300

    add header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,16 +4,18 @@ import { FormContact } from './FormContact/FormContact';
 import { ListContacts } from './ListContacts/ListContacts';
 import { Filter } from './Filter/Filter';
 import { Loader } from './Loader/Loader';
+import { Header } from './Header/Header';
 import css from './App.module.css';
 import { selectIsLoading, selectError } from '../redux/contactsSlice';
 
 export const App = () => {
-    const isLoading = useSelector(selectIsLoading);
-    const error = useSelector(selectError);
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
 
   return (
     <div className={css.appwrap}>
-      {isLoading && !error && <Loader/>}
+      <Header />
+      {isLoading && !error && <Loader />}
       <h1> Phonebook</h1>
       <FormContact />
       <h2>Contacts</h2>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+export const Header = () => {
+  return (
+    <div>
+      <div>
+        <a href="/">my CONTACTS</a>
+      </div>
+
+      <div>
+        <p>email</p>
+        <button>Log Out</button>
+      </div>
+    </div>
+  );
+};
